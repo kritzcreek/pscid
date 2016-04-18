f4943df1fc0c6ee8c1a54de6ef61c7866d5fc2c0
Christoph Hegemann <christoph.hegemann1337@gmail.com>
2016-04-18 02:31:35 +0200

    use psa to format errors and warnings

--- src/Main.purs
+++ src/Main.purs
@@ -8,15 +8,16 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE)
 import Control.Monad.Eff.Exception (EXCEPTION)
-import Data.Array (head)
+import Control.Monad.Eff.Unsafe (unsafeInterleaveEff)
 import Data.Either (either)
 import Data.Either.Unsafe (fromRight)
 import Data.Function.Eff (runEffFn2, EffFn2)
-import Data.Maybe (maybe, Maybe(Nothing, Just))
+import Data.Maybe (Maybe(Nothing, Just))
 import Node.ChildProcess (ChildProcess, CHILD_PROCESS)
 import Node.FS (FS)
-import PscIde (load, rebuild, cwd, NET)
-import PscIde.Command (RebuildResult(RebuildResult), RebuildError(RebuildError), Message(Message))
+import PscIde (sendCommandR, load, cwd, NET)
+import PscIde.Command (Command(RebuildCmd), Message(Message))
+import Pscid.Psa (psaPrinter)
 import Pscid.Server (ServerStartResult(..), startServer)
 
 port ∷ Int
@@ -29,40 +30,28 @@ main = launchAff do
   mCp ← serverRunning <$> startServer "psc-ide-server" 4243
   load port [] []
   Message directory ← fromRight <$> cwd port
-  liftEff' $ runEffFn2 gaze (directory <> "/src/**/*.purs") (rebuildStuff port)
+  liftEff' $ runEffFn2 gaze (directory <> "/src/**/*.purs") (rebuildStuff port directory)
   liftEff $ clearConsole
-  log "Watching O.O"
+  log "Watching \\O/"
   pure unit
 
 rebuildStuff
   ∷ ∀ e
     . Int
     → String
-    → Eff ( net :: NET , console :: CONSOLE , err :: EXCEPTION | e) Unit
-rebuildStuff p file = launchAff do
-  errs ← fromRight <$> rebuild p file
+    → String
+    → Eff ( net :: NET
+          , console :: CONSOLE
+          , err :: EXCEPTION | e) Unit
+
+rebuildStuff p dir file = dropFS $ launchAff do
+  errs ← fromRight <$> sendCommandR p (RebuildCmd file)
   liftEff clearConsole
-  log (file <>
-       "\n≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡\n" <>
-       (either prettyError prettyWarning errs))
+  log ("Checking " <> file)
+  liftEff' $ either (psaPrinter true file) (psaPrinter false file) errs
 
-prettyError :: RebuildResult -> String
-prettyError rr = "ERROR:\n" <> prettyRebuildResult rr
-
-prettyWarning :: RebuildResult -> String
-prettyWarning (RebuildResult []) = "YOU ARE AWESOME!"
-prettyWarning rr = "WARNING:\n" <> prettyRebuildResult rr
-
-prettyRebuildResult ∷ RebuildResult -> String
-prettyRebuildResult (RebuildResult errs) = maybe "" pr (head errs)
-  where pr (RebuildError {message, position}) =
-          case position of
-               Nothing ->
-                 message
-               Just {line, column} ->
-                 "line: " <> show line <>
-                 ", column: " <> show column <>
-                 "\n" <> message
+dropFS ∷ ∀ e a. Eff (fs ∷ FS | e) a → Eff e a
+dropFS = unsafeInterleaveEff
 
 serverRunning ∷ ServerStartResult → Maybe ChildProcess
 serverRunning (Started cp) = Just cp
@@ -71,4 +60,3 @@ serverRunning (StartError e) = Nothing
 
 foreign import gaze ∷ ∀ eff. EffFn2 (fs ∷ FS | eff) String (String → Eff eff Unit) Unit
 foreign import clearConsole ∷ ∀ e. Eff (console ∷ CONSOLE | e) Unit
--- foreign import gaze ∷ ∀ eff. String → (∀ e. String → Eff e Unit)Eff (fs ∷ FS | eff) String (String → Eff eff Unit) Unit
---
+++ src/Pscid/Psa.purs
@@ -0,0 +1,93 @@
+module Pscid.Psa where
+
+import Prelude
+import Control.Monad.Eff.Console as Console
+import Data.Array as Array
+import Data.Set as Set
+import Data.String as Str
+import Node.Encoding as Encoding
+import Node.FS.Sync as File
+import Control.Monad (when)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console.Unsafe (logAny)
+import Control.Monad.Eff.Exception (EXCEPTION)
+import Data.Argonaut (Json)
+import Data.Argonaut.Decode (decodeJson)
+import Data.Array (head, null)
+import Data.Either (Either(..))
+import Data.Maybe (Maybe(..))
+import Data.StrMap (StrMap)
+import Data.String (joinWith)
+import Data.Traversable (traverse)
+import Node.FS (FS)
+import Psa (Output, PsaResult, parsePsaError, PsaOptions, output)
+import Psa.Printer (renderAnsi, renderRow)
+import Psa.Printer.Default (renderError, renderWarning)
+import Psa.Util (iter_)
+
+defaultOptions :: PsaOptions
+defaultOptions =
+  { ansi: true
+  , censorWarnings: false
+  , censorLib: false
+  , censorSrc: false
+  , censorCodes: Set.empty
+  , filterCodes: Set.empty
+  , verboseStats: false
+  , libDirs: []
+  , strict: false
+  , cwd: ""
+  }
+
+type MainEff eff =
+  ( console :: Console.CONSOLE
+  , err :: EXCEPTION
+  , fs :: FS
+  | eff
+  )
+
+print :: forall eff. PsaOptions -> Output -> Eff (console :: Console.CONSOLE | eff) Unit
+print options {warnings, errors} = do
+  iter_ warnings \i warning -> do
+    Console.error $ toString (renderWarning 1 1 warning)
+    Console.error ""
+
+  when (null errors) (Console.error "NO ERRORS?!?!?!")
+
+  iter_ errors \i error -> do
+    Console.error $ toString (renderError 1 1 error)
+    Console.error ""
+
+  -- Console.error $ toString (renderStats' output.stats)
+
+  where
+  toString = renderRow (joinWith "" <<< map (renderAnsi options.ansi))
+
+parsePscidResult :: Boolean -> Array (StrMap Json) -> Either String PsaResult
+parsePscidResult isError obj =
+  (if isError
+  then { warnings: []
+       , errors: _
+       }
+  else { warnings: _
+       , errors: []
+       }) <$> maybeToArray <<< head <$> traverse parsePsaError obj
+  where
+    maybeToArray (Just a) = [a]
+    maybeToArray Nothing = []
+
+psaPrinter :: forall eff. Boolean -> String -> Json -> Eff (MainEff eff) Unit
+psaPrinter isError file err = do
+  case decodeJson err >>= parsePscidResult isError of
+    Left _ -> logAny err
+    Right out -> do
+      let filenames = Set.singleton file
+      out' <- output loadLines defaultOptions out
+      print defaultOptions out'
+
+      where
+      -- TODO: Handle exceptions
+      loadLines filename pos = do
+        contents <- Str.split "\n" <$> File.readTextFile Encoding.UTF8 filename
+        let source = Array.slice (pos.startLine - 1) (pos.endLine) contents
+        pure $ Just source
