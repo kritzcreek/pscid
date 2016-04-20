961f1dc632810c2a3fa0df39fca47819627cf89f
Christoph Hegemann <christoph.hegemann1337@gmail.com>
2016-04-20 23:38:25 +0200

    add restart command and error handling

--- src/Main.purs
+++ src/Main.purs
@@ -5,7 +5,8 @@ import Control.Monad.Eff.Console as Console
 import Node.Process as Process
 import Control.Apply ((*>))
 import Control.Bind ((=<<))
-import Control.Monad.Aff (runAff, launchAff)
+import Control.Monad (when)
+import Control.Monad.Aff (attempt, runAff, launchAff)
 import Control.Monad.Aff.AVar (AVAR)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
@@ -17,7 +18,7 @@ import Control.Monad.ST (readSTRef, modifySTRef, newSTRef, runST)
 import Control.Monad.Trans (lift)
 import Data.Argonaut (Json)
 import Data.Array (uncons)
-import Data.Either (Either, either)
+import Data.Either (isLeft, Either(), either)
 import Data.Either.Unsafe (fromRight)
 import Data.Function.Eff (runEffFn2, EffFn2)
 import Data.Functor (($>))
@@ -32,7 +33,7 @@ import PscIde.Command (Command(RebuildCmd), Message(Message))
 import Pscid.Keypress (Key(Key), onKeypress, initializeKeypresses)
 import Pscid.Options (PscidOptions, optionParser)
 import Pscid.Psa (psaPrinter)
-import Pscid.Server (stopServer, startServer)
+import Pscid.Server (restartServer, stopServer, startServer)
 
 infixr 9 compose as ∘
 
@@ -43,7 +44,9 @@ main ∷ ∀ e. Eff ( err ∷ EXCEPTION, cp ∷ CHILD_PROCESS
                 , avar ∷ AVAR, fs ∷ FS, process ∷ Process.PROCESS | e) Unit
 main = launchAff do
   config@{port} ← liftEff optionParser
-  _ ← startServer "psc-ide-server" port
+  liftEff (log "Starting psc-ide-server")
+  r ← attempt $ startServer "psc-ide-server" port
+  when (isLeft r) (restartServer port)
   load port [] []
   Message directory ← fromRight <$> cwd port
   liftEff do
@@ -56,6 +59,7 @@ main = launchAff do
     log ("Watching " <> directory <> " on port " <> show port)
     log owl
     log "Press b to build (tries \"npm run build\" then \"pulp build\")"
+    log "Press r to reset"
     log "Press q to quit"
 
 owl :: String
@@ -69,16 +73,20 @@ owl =
 
 keyHandler ∷ ∀ e. Key → Pscid ( console ∷ CONSOLE , cp ∷ CHILD_PROCESS
                               , process ∷ Process.PROCESS , net ∷ NET
-                              , fs ∷ FS | e) Unit
+                              , fs ∷ FS, avar ∷ AVAR | e) Unit
 keyHandler k = do
   {port} ← ask
   case k of
     Key {ctrl: false, name: "b", meta: false, shift: false} →
       buildProject
+    Key {ctrl: false, name: "r", meta: false, shift: false} →
+      liftEff ∘ catchLog "Failed to restart server" $ launchAff do
+        restartServer port
+        load port [] []
     Key {ctrl: false, name: "q", meta: false, shift: false} →
-      liftEff (Console.log "Bye!" *> runAff exit exit (stopServer port))
-    Key {ctrl, name, meta, shift}→
-      liftEff (Console.log name)
+      liftEff (log "Bye!" *> runAff exit exit (stopServer port))
+    Key {ctrl, name, meta, shift} →
+      liftEff (log name)
   where
     exit ∷ ∀ a eff. a → Eff (process ∷ Process.PROCESS | eff) Unit
     exit = const (Process.exit 0)
@@ -86,7 +94,7 @@ keyHandler k = do
 buildProject ∷ ∀ e. Pscid (cp ∷ CHILD_PROCESS, console ∷ CONSOLE | e) Unit
 buildProject = do
   {buildCommand} ← ask
-  liftEff $ catchException (const (Console.log "Build Project threw an exception")) $
+  liftEff ∘ catchLog "Build Project threw an exception" $
     runST do
       let cmd = fromJust (uncons (split " " buildCommand))
       output ← newSTRef ""
@@ -110,20 +118,19 @@ buildProject = do
         BySignal _       → pure unit
 
 triggerRebuild
-  ∷ ∀ e . String → Pscid ( net ∷ NET, console ∷ CONSOLE, fs ∷ FS | e) Unit
+  ∷ ∀ e . String → Pscid (net ∷ NET, console ∷ CONSOLE, fs ∷ FS | e) Unit
 triggerRebuild file = do
   {port} ← ask
-  lift $ runAff
-    (const (log "We couldn't talk to the server"))
-    (printRebuildResult file)
-    (fromRight <$> sendCommandR port (RebuildCmd file))
+  lift ∘ catchLog "We couldn't talk to the server" $ launchAff do
+    errs ← fromRight <$> sendCommandR port (RebuildCmd file)
+    liftEff (printRebuildResult file errs)
 
 printRebuildResult
   ∷ ∀ e. String
     → Either Json Json
     → Eff (console ∷ CONSOLE, fs ∷ FS | e) Unit
 printRebuildResult file errs =
-  catchException (const (Console.error "An error inside psaPrinter")) do
+  catchLog "An error inside psaPrinter" do
     clearConsole
     Console.log ("Checking " <> file)
     either (psaPrinter owl true file) (psaPrinter owl false file) errs
@@ -131,3 +138,10 @@ printRebuildResult file errs =
 foreign import gaze
   ∷ ∀ eff. EffFn2 (fs ∷ FS | eff) String (String → Eff (fs ∷ FS | eff) Unit) Unit
 foreign import clearConsole ∷ ∀ e. Eff (console ∷ CONSOLE | e) Unit
+
+catchLog
+  ∷ ∀ e
+  . String
+  → Eff (console ∷ CONSOLE, err ∷ EXCEPTION | e) Unit
+  → Eff (console ∷ CONSOLE | e) Unit
+catchLog m = catchException (const (Console.error m))
