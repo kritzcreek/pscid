3daeed0771729bcfbf364be1c7c5f87b6b5f636a
Christoph Hegemann <kRITZCREEK@users.noreply.github.com>
2016-04-20 23:54:52 +0200

    Restart keybinding and more error handling (#5)
    * Use Reader for Options
    
    * add restart command and error handling

--- src/Main.purs
+++ src/Main.purs
@@ -3,18 +3,22 @@ module Main where
 import Prelude
 import Control.Monad.Eff.Console as Console
 import Node.Process as Process
+import Control.Apply ((*>))
 import Control.Bind ((=<<))
-import Control.Monad.Aff (runAff, launchAff)
+import Control.Monad (when)
+import Control.Monad.Aff (attempt, runAff, launchAff)
 import Control.Monad.Aff.AVar (AVAR)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (log, CONSOLE)
 import Control.Monad.Eff.Exception (catchException, EXCEPTION)
-import Control.Monad.Eff.Unsafe (unsafeInterleaveEff)
+import Control.Monad.Reader.Class (ask)
+import Control.Monad.Reader.Trans (runReaderT, ReaderT)
 import Control.Monad.ST (readSTRef, modifySTRef, newSTRef, runST)
+import Control.Monad.Trans (lift)
 import Data.Argonaut (Json)
 import Data.Array (uncons)
-import Data.Either (Either, either)
+import Data.Either (isLeft, Either(), either)
 import Data.Either.Unsafe (fromRight)
 import Data.Function.Eff (runEffFn2, EffFn2)
 import Data.Functor (($>))
@@ -27,28 +31,35 @@ import Node.Stream (onDataString)
 import PscIde (sendCommandR, load, cwd, NET)
 import PscIde.Command (Command(RebuildCmd), Message(Message))
 import Pscid.Keypress (Key(Key), onKeypress, initializeKeypresses)
-import Pscid.Options (optionParser)
+import Pscid.Options (PscidOptions, optionParser)
 import Pscid.Psa (psaPrinter)
-import Pscid.Server (stopServer, startServer)
+import Pscid.Server (restartServer, stopServer, startServer)
 
 infixr 9 compose as ∘
+
+type Pscid e a = ReaderT PscidOptions (Eff e) a
 
 main ∷ ∀ e. Eff ( err ∷ EXCEPTION, cp ∷ CHILD_PROCESS
                 , console ∷ CONSOLE , net ∷ NET
                 , avar ∷ AVAR, fs ∷ FS, process ∷ Process.PROCESS | e) Unit
 main = launchAff do
-  {port, buildCommand, testCommand, buildAfterSave, testAfterSave} ← liftEff optionParser
-  _ ← startServer "psc-ide-server" port
+  config@{port} ← liftEff optionParser
+  liftEff (log "Starting psc-ide-server")
+  r ← attempt $ startServer "psc-ide-server" port
+  when (isLeft r) (restartServer port)
   load port [] []
   Message directory ← fromRight <$> cwd port
   liftEff do
-    (runEffFn2 gaze (directory <> "/src/**/*.purs") (triggerRebuild port))
+    runEffFn2 gaze
+      (directory <> "/src/**/*.purs")
+      (\d → runReaderT (triggerRebuild d) config)
     clearConsole
     initializeKeypresses
-    (onKeypress (keyHandler port buildCommand))
+    onKeypress (\k → runReaderT (keyHandler k) config)
     log ("Watching " <> directory <> " on port " <> show port)
     log owl
     log "Press b to build (tries \"npm run build\" then \"pulp build\")"
+    log "Press r to reset"
     log "Press q to quit"
 
 owl :: String
@@ -60,23 +71,34 @@ owl =
  -"-"-   -"-"-                 -"-"-   -"-"-
   """
 
-keyHandler ∷ ∀ e. Int → String → Key → Eff ( console ∷ CONSOLE , cp ∷ CHILD_PROCESS
-                                             , process ∷ Process.PROCESS , net ∷ NET
-                                             , fs ∷ FS | e) Unit
-keyHandler port buildCommand k = case k of
-  Key {ctrl: false, name: "b", meta: false, shift: false} → buildProject buildCommand
-  Key {ctrl: false, name: "q", meta: false, shift: false} → do
-    Console.log "Bye!"
-    runAff (const (Process.exit 0)) (const (Process.exit 0)) (stopServer port)
-  Key {ctrl, name, meta, shift}                           → Console.log name
+keyHandler ∷ ∀ e. Key → Pscid ( console ∷ CONSOLE , cp ∷ CHILD_PROCESS
+                              , process ∷ Process.PROCESS , net ∷ NET
+                              , fs ∷ FS, avar ∷ AVAR | e) Unit
+keyHandler k = do
+  {port} ← ask
+  case k of
+    Key {ctrl: false, name: "b", meta: false, shift: false} →
+      buildProject
+    Key {ctrl: false, name: "r", meta: false, shift: false} →
+      liftEff ∘ catchLog "Failed to restart server" $ launchAff do
+        restartServer port
+        load port [] []
+    Key {ctrl: false, name: "q", meta: false, shift: false} →
+      liftEff (log "Bye!" *> runAff exit exit (stopServer port))
+    Key {ctrl, name, meta, shift} →
+      liftEff (log name)
+  where
+    exit ∷ ∀ a eff. a → Eff (process ∷ Process.PROCESS | eff) Unit
+    exit = const (Process.exit 0)
 
-buildProject ∷ ∀ e. String → Eff (cp ∷ CHILD_PROCESS, console ∷ CONSOLE | e) Unit
-buildProject buildScript =
-  catchException (const (Console.log "Build Project threw an exception")) $
+buildProject ∷ ∀ e. Pscid (cp ∷ CHILD_PROCESS, console ∷ CONSOLE | e) Unit
+buildProject = do
+  {buildCommand} ← ask
+  liftEff ∘ catchLog "Build Project threw an exception" $
     runST do
-      let cmd = fromJust (uncons (split " " buildScript))
+      let cmd = fromJust (uncons (split " " buildCommand))
       output ← newSTRef ""
-      log ("Running: \"" <> buildScript <> "\"")
+      log ("Running: \"" <> buildCommand <> "\"")
       cp ← spawn cmd.head cmd.tail defaultSpawnOptions
 
       let stout = stdout cp
@@ -96,26 +118,30 @@ buildProject buildScript =
         BySignal _       → pure unit
 
 triggerRebuild
-  ∷ ∀ e . Int → String → Eff ( net ∷ NET , console ∷ CONSOLE | e) Unit
-triggerRebuild p file = dropFS do
-  runAff
-    (const (log "We couldn't talk to the server"))
-    (printRebuildResult file)
-    (fromRight <$> sendCommandR p (RebuildCmd file))
-  where
-    dropFS ∷ ∀ eff a. Eff (fs ∷ FS | eff) a → Eff eff a
-    dropFS = unsafeInterleaveEff
+  ∷ ∀ e . String → Pscid (net ∷ NET, console ∷ CONSOLE, fs ∷ FS | e) Unit
+triggerRebuild file = do
+  {port} ← ask
+  lift ∘ catchLog "We couldn't talk to the server" $ launchAff do
+    errs ← fromRight <$> sendCommandR port (RebuildCmd file)
+    liftEff (printRebuildResult file errs)
 
 printRebuildResult
   ∷ ∀ e. String
     → Either Json Json
     → Eff (console ∷ CONSOLE, fs ∷ FS | e) Unit
 printRebuildResult file errs =
-  catchException (const (Console.error "An error inside psaPrinter")) do
+  catchLog "An error inside psaPrinter" do
     clearConsole
     Console.log ("Checking " <> file)
     either (psaPrinter owl true file) (psaPrinter owl false file) errs
 
 foreign import gaze
-  ∷ ∀ eff. EffFn2 (fs ∷ FS | eff) String (String → Eff eff Unit) Unit
+  ∷ ∀ eff. EffFn2 (fs ∷ FS | eff) String (String → Eff (fs ∷ FS | eff) Unit) Unit
 foreign import clearConsole ∷ ∀ e. Eff (console ∷ CONSOLE | e) Unit
+
+catchLog
+  ∷ ∀ e
+  . String
+  → Eff (console ∷ CONSOLE, err ∷ EXCEPTION | e) Unit
+  → Eff (console ∷ CONSOLE | e) Unit
+catchLog m = catchException (const (Console.error m))
--- src/Pscid/Server.purs
+++ src/Pscid/Server.purs
@@ -1,12 +1,14 @@
 module Pscid.Server where
 
 import Prelude
-import Control.Monad.Eff.Class (liftEff)
-import Control.Monad.Eff.Console (CONSOLE, log)
-import Control.Monad.Aff (Aff, later', makeAff)
+import Node.Process as Process
+import Control.Alt ((<|>))
+import Control.Monad.Aff (attempt, Aff, later', makeAff)
 import Control.Monad.Aff.AVar (AVAR)
 import Control.Monad.Aff.Par (Par(Par), runPar)
-import Control.Alt ((<|>))
+import Control.Monad.Eff.Class (liftEff)
+import Control.Monad.Eff.Console (CONSOLE, log)
+import Data.Either (Either(Right, Left))
 import Node.ChildProcess (CHILD_PROCESS, ChildProcess, Exit(Normally), onClose, onError, defaultSpawnOptions, spawn)
 import PscIde (NET, quit)
 
@@ -17,14 +19,13 @@ data ServerStartResult =
 
 -- | Start a psc-ide server instance, or find one already running on the expected port, checking if it has the right path.
 startServer
-  :: forall eff
+  ∷ forall eff
   . String
-  -> Int
-  -> Aff (cp :: CHILD_PROCESS, console :: CONSOLE, net :: NET, avar :: AVAR | eff) ServerStartResult
+  → Int
+  → Aff (cp ∷ CHILD_PROCESS, console ∷ CONSOLE, net ∷ NET, avar ∷ AVAR | eff) ServerStartResult
 startServer exe port = do
-    liftEff $ log "Starting psc-ide-server"
-    cp <- liftEff $ spawn exe ["-p", show port] defaultSpawnOptions
-    let handleErr = makeAff $ \_ succ -> do
+    cp <- liftEff (spawn exe ["-p", show port] defaultSpawnOptions)
+    let handleErr = makeAff \_ succ -> do
                       onError cp (\_ -> succ $ StartError "psc-ide-server error")
                       onClose cp (\exit -> case exit of
                                      (Normally 0) -> succ Closed
@@ -32,6 +33,22 @@ startServer exe port = do
                                      _ -> succ $ StartError "Other close error")
 
     runPar (Par handleErr <|> Par (later' 100 $ pure $ Started cp))
+
+restartServer
+  ∷ forall e
+    . Int
+    → Aff ( cp ∷ CHILD_PROCESS , net ∷ NET
+          , console ∷ CONSOLE , avar ∷ AVAR
+          , process ∷ Process.PROCESS | e) Unit
+restartServer port = do
+  attempt (stopServer port)
+  r ← attempt (startServer "psc-ide-server" port)
+  liftEff case r of
+    Left e → do
+      log ("Failed to restart psc-ide-server on port: " <> show port
+           <> "\nThe error was: " <> show e)
+      Process.exit 1
+    Right _ → log "I restarted psc-ide-server for you."
 
 -- | Stop a psc-ide server. Currently implemented by asking it nicely, but potentially by killing it if that doesn't work...
 stopServer :: forall eff. Int -> Aff (cp :: CHILD_PROCESS, net :: NET | eff) Unit
