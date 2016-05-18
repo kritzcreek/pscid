dbc87bd0377e388132feda5d97fbb825d3b65de5
Christoph Hegemann <christoph.hegemann1337@gmail.com>
2016-05-18 14:51:27 +0200

    apply suggestions on s

--- src/Main.purs
+++ src/Main.purs
@@ -3,7 +3,7 @@ module Main where
 import Prelude
 import Control.Monad.Eff.Console as Console
 import Node.Process as Process
-import Ansi.Codes (Color(Green, Red))
+import Ansi.Codes (Color(Blue, Green, Red))
 import Ansi.Output (foreground, withGraphics)
 import Control.Apply ((*>))
 import Control.Bind ((=<<))
@@ -14,18 +14,18 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (log, CONSOLE)
 import Control.Monad.Eff.Exception (catchException, EXCEPTION)
+import Control.Monad.Eff.Ref (writeRef, readRef, Ref, newRef, REF)
 import Control.Monad.Reader.Class (ask)
 import Control.Monad.Reader.Trans (runReaderT, ReaderT)
 import Control.Monad.ST (readSTRef, modifySTRef, newSTRef, runST)
 import Control.Monad.Trans (lift)
 import Data.Argonaut (Json)
-import Data.Array (filter, uncons)
+import Data.Array (head, filter, uncons)
 import Data.Either (isRight, isLeft, Either(Left, Right), either)
-import Data.Either.Unsafe (fromRight)
 import Data.Foldable (notElem)
 import Data.Function.Eff (runEffFn2, EffFn2)
 import Data.Functor (($>))
-import Data.Maybe (Maybe(Nothing))
+import Data.Maybe (Maybe(Just, Nothing))
 import Data.Maybe.Unsafe (fromJust)
 import Data.String (split)
 import Node.ChildProcess (CHILD_PROCESS, stderr, stdout, Exit(BySignal, Normally), onExit, defaultSpawnOptions, spawn)
@@ -39,30 +39,43 @@ import Pscid.Options (PscidOptions, optionParser)
 import Pscid.Psa (PsaError, parseErrors, psaPrinter)
 import Pscid.Server (restartServer, stopServer, startServer)
 import Pscid.Util ((∘))
+import Suggest (applySuggestions)
 
 type Pscid e a = ReaderT PscidOptions (Eff e) a
 
+newtype State = State { errors :: Array PsaError }
+
+emptyState :: State
+emptyState = State { errors: [] }
+
 main ∷ ∀ e. Eff ( err ∷ EXCEPTION, cp ∷ CHILD_PROCESS
                 , console ∷ CONSOLE , net ∷ NET
-                , avar ∷ AVAR, fs ∷ FS, process ∷ Process.PROCESS | e) Unit
+                , avar ∷ AVAR, fs ∷ FS, process ∷ Process.PROCESS
+                , ref :: REF | e) Unit
 main = launchAff do
   config@{ port, sourceDirectories } ← liftEff optionParser
+  stateRef <- liftEff (newRef emptyState)
   liftEff (log "Starting psc-ide-server")
   r ← attempt (startServer "psc-ide-server" port Nothing)
   when (isLeft r) (restartServer port)
   Message directory ← later' 100 do
     load port [] []
-    fromRight <$> cwd port
+    res ← cwd port
+    case res of
+      Right d → pure d
+      Left err → liftEff do
+        log err
+        Process.exit 1
   liftEff do
     runEffFn2 gaze
       (sourceDirectories <#> \g → directory <> "/" <> g <> "/**/*.purs")
-      (\d → runReaderT (triggerRebuild d) config)
+      (\d → runReaderT (triggerRebuild stateRef d) config)
     clearConsole
     initializeKeypresses
-    onKeypress (\k → runReaderT (keyHandler k) config)
+    onKeypress (\k → runReaderT (keyHandler stateRef k) config)
     log ("Watching " <> directory <> " on port " <> show port)
     log owl
-    log helpText
+    logColored Blue helpText
 
 owl ∷ String
 owl =
@@ -82,11 +95,14 @@ Press r to reset
 Press q to quit
   """
 
-
-keyHandler ∷ ∀ e. Key → Pscid ( console ∷ CONSOLE , cp ∷ CHILD_PROCESS
-                              , process ∷ Process.PROCESS , net ∷ NET
-                              , fs ∷ FS, avar ∷ AVAR | e) Unit
-keyHandler k = do
+keyHandler
+  ∷ ∀ e
+  . Ref State
+  → Key
+  → Pscid ( console ∷ CONSOLE , cp ∷ CHILD_PROCESS
+          , process ∷ Process.PROCESS , net ∷ NET
+          , fs ∷ FS, avar ∷ AVAR, ref ∷ REF | e) Unit
+keyHandler stateRef k = do
   {port, buildCommand, testCommand} ← ask
   case k of
     Key {ctrl: false, name: "b", meta: false, shift: false} →
@@ -99,6 +115,13 @@ keyHandler k = do
         restartServer port
         load port [] []
       log owl
+    Key {ctrl: false, name: "s", meta: false, shift: false} → liftEff do
+      State state ← readRef stateRef
+      case head state.errors of
+        Nothing →
+          log "No suggestions available"
+        Just e →
+          catchLog "Couldn't apply suggestion." (runST (applySuggestions [e]))
     Key {ctrl: false, name: "q", meta: false, shift: false} →
       liftEff (log "Bye!" *> runAff exit exit (stopServer port))
     Key {ctrl, name, meta, shift} →
@@ -137,33 +160,47 @@ runCommand name command =
         BySignal _       → pure unit
 
 triggerRebuild
-  ∷ ∀ e . String → Pscid ( cp ∷ CHILD_PROCESS, net ∷ NET
-                         , console ∷ CONSOLE, fs ∷ FS | e) Unit
-triggerRebuild file = do
+  ∷ ∀ e
+  . Ref State
+  → String
+  → Pscid ( cp ∷ CHILD_PROCESS, net ∷ NET
+          , console ∷ CONSOLE, fs ∷ FS
+          , ref ∷ REF| e) Unit
+triggerRebuild stateRef file = do
   {port, testCommand, testAfterRebuild, censorCodes} ← ask
   lift ∘ catchLog "We couldn't talk to the server" $ launchAff do
-    errs ← fromRight <$> sendCommandR port (RebuildCmd file)
-    liftEff (handleRebuildResult file censorCodes errs)
-    liftEff ∘ when (testAfterRebuild && isRight errs) $
-      runCommand "Test" testCommand
+    result ← sendCommandR port (RebuildCmd file)
+    case result of
+      Left _ → liftEff (log "We couldn't talk to the server")
+      Right errs → do
+        parsedErrors ← liftEff (handleRebuildResult file censorCodes errs)
+        liftEff (writeRef stateRef (State {errors: parsedErrors}))
+        case head parsedErrors >>= _.suggestion of
+          Nothing → pure unit
+          Just s → liftEff (logColored Blue "Press s to automatically apply the suggestion.")
+        liftEff ∘ when (testAfterRebuild && isRight errs) $
+          runCommand "Test" testCommand
 
 handleRebuildResult
   ∷ ∀ e
   . String
   → Array String
   → Either Json Json
-  → Eff (console ∷ CONSOLE, fs ∷ FS | e) Unit
+  → Eff (console ∷ CONSOLE, fs ∷ FS | e) (Array PsaError)
 handleRebuildResult file censorCodes result = do
   clearConsole
   log ("Checking " <> file)
   case both parseErrors result of
     Right warnings →
       either
-        (const (log "Failed to parse warnings"))
-        (psaPrinter owl false)
+        (\_ → log "Failed to parse warnings" $> [])
+        (\e → psaPrinter owl false e $> e)
         (filterWarnings censorCodes <$> warnings)
     Left errors →
-      either (const (log "Failed to parse errors")) (psaPrinter owl true) errors
+      either
+        (\_ → log "Failed to parse errors" $> [])
+        (\e → psaPrinter owl true e $> e)
+        errors
 
 filterWarnings ∷ Array String → Array PsaError → Array PsaError
 filterWarnings ignored errors =
