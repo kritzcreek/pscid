eb682c68de975ac7dcfecc9ff2e1eb791141b836
kritzcreek <christoph.hegemann1337@gmail.com>
2019-03-13 09:40:24 +0100

    Updates dependencies
    some minor cleanup

--- src/Pscid/Options.purs
+++ src/Pscid/Options.purs
@@ -8,8 +8,6 @@ import Data.Array as Array
 import Data.Either (Either(..))
 import Data.Int as Int
 import Data.Maybe (Maybe(..), fromMaybe)
-import Data.Newtype (class Newtype, un)
-import Data.Newtype as Newtype
 import Data.String as String
 import Effect (Effect)
 import Effect.Console as Console
@@ -21,7 +19,7 @@ import Node.Yargs.Applicative (flag, runY, yarg)
 import Node.Yargs.Setup (defaultHelp, defaultVersion, example, usage)
 import Pscid.Util ((∘))
 
-newtype PscidSettings a = PscidSettings
+type PscidSettings a =
   { port              ∷ a
   , buildCommand      ∷ CLICommand
   , outputDirectory   ∷ String
@@ -31,12 +29,10 @@ newtype PscidSettings a = PscidSettings
   , censorCodes       ∷ Array String
   }
 
-derive instance newtypePscidSetting ∷ Newtype (PscidSettings a) _
-
 type PscidOptions = PscidSettings (Maybe Int)
 
 defaultOptions ∷ PscidOptions
-defaultOptions = PscidSettings
+defaultOptions =
   { port: Nothing
   , buildCommand: PulpCommand (pulpCmd <> " build") []
   , outputDirectory: "output"
@@ -64,13 +60,10 @@ npmCmd = if platform == Just Win32 then "npm.cmd" else "npm"
 
 mkDefaultOptions ∷ Effect PscidOptions
 mkDefaultOptions =
-  Newtype.traverse PscidSettings collectData defaultOptions
-  where
-    collectData x =
-      x {buildCommand = _, testCommand = _, sourceDirectories = _}
-      <$> mkCommand "build"
-      <*> mkCommand "test"
-      <*> scanDefaultDirectories
+  (defaultOptions { buildCommand = _, testCommand = _, sourceDirectories = _ })
+    <$> mkCommand "build"
+    <*> mkCommand "test"
+    <*> scanDefaultDirectories
 
 type IncludePath = String
 
@@ -78,29 +71,29 @@ data CLICommand
   = ScriptCommand String
   | PulpCommand String (Array IncludePath)
 
-printCLICommand :: CLICommand -> String
+printCLICommand ∷ CLICommand → String
 printCLICommand = case _ of
-  ScriptCommand str -> str
-  PulpCommand str includesArr ->
-    if Array.null includesArr then
-      str
-    else
-      str <> " -I " <> (String.joinWith ":" includesArr)
+  ScriptCommand str →
+    str
+  PulpCommand str [] →
+    str
+  PulpCommand str includes →
+    str <> " -I " <> (String.joinWith ":" includes)
 
 -- | If the command is a PulpCommand (eg. "pulp build"), then the array of
 -- | include paths is set. If the command is an NPM script, the command is
 -- | left unchanged. This is because it's impossible to guarantee that the NPM
 -- | script directly executes "pulp build" (it may execute another script), and
 -- | therefore we cannot simply append the includes onto the end of the command.
-setCommandIncludes :: Array IncludePath -> CLICommand -> CLICommand
+setCommandIncludes ∷ Array IncludePath → CLICommand → CLICommand
 setCommandIncludes includesArr cmd = case cmd of
-  PulpCommand str _ -> PulpCommand str includesArr
-  ScriptCommand str -> ScriptCommand str
+  PulpCommand str _ → PulpCommand str includesArr
+  ScriptCommand str → ScriptCommand str
 
 mkCommand ∷ String → Effect CLICommand
 mkCommand cmd = do
   pscidSpecific ← hasNamedScript ("pscid:" <> cmd)
-  namedScript   ← hasNamedScript cmd
+  namedScript ← hasNamedScript cmd
 
   let specificCommand =
         guard pscidSpecific $> ScriptCommand ("npm run -s pscid:" <> cmd)
@@ -117,9 +110,9 @@ optionParser ∷ Effect PscidOptions
 optionParser =
   let
     setup = usage "$0 -p 4245"
-            <> example "$0 -p 4245" "Watching ... on port 4245"
-            <> defaultHelp
-            <> defaultVersion
+      <> example "$0 -p 4245" "Watching ... on port 4245"
+      <> defaultHelp
+      <> defaultVersion
   in
    catchException (const do
                       Console.error "Failed parsing the arguments."
@@ -149,14 +142,14 @@ buildOptions
   → String
   → Effect PscidOptions
 buildOptions port testAfterRebuild includes outputDirectory censor = do
-  defaults ← un PscidSettings <$> mkDefaultOptions
+  defaults ← mkDefaultOptions
   let includesArr = Array.filter (not String.null) (String.split (String.Pattern ";") includes)
       sourceDirectories = defaults.sourceDirectories <> includesArr
       censorCodes = Array.filter (not String.null) (String.split (String.Pattern ",") censor)
       buildCommand = setCommandIncludes includesArr defaults.buildCommand
       testCommand = setCommandIncludes includesArr defaults.testCommand
 
-  pure (PscidSettings
+  pure
     { port: Int.fromNumber (readInt 10 port)
     , testAfterRebuild
     , sourceDirectories
@@ -164,7 +157,7 @@ buildOptions port testAfterRebuild includes outputDirectory censor = do
     , buildCommand
     , outputDirectory
     , testCommand
-    })
+    }
 
 foreign import hasNamedScript ∷ String → Effect Boolean
 foreign import glob ∷ String → Effect (Array String)
